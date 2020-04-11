022c42426f4dacdad11dff4a99ffd9699e09216d
TebogoMothelo <tebogo.mothelo@umuzi.org>
2020-04-11 18:32:26 +0200

    added cases for ICU and ventilators

--- src/functions.js
+++ src/functions.js
@@ -12,35 +12,9 @@ const input = {
     totalHospitalBeds: 1380614 } 
 
 const impacts = (data) => {
-    
+    //currentlyinfected
     const currentlyInfected = data.reportedCases * 10;
-    let duration = 0;
-    if(data.periodType == "days"){
-        duration = data.timeToElapse;
-    } else if(data.periodType == "weeks"){
-        duration = data.timeToElapse * 7;
-    } else if(data.periodType == "months"){
-        duration = data.timeToElapse * 30;
-    } else {
-        duration = data.timeToElapse * 365;
-    }
-    let factor = duration/3;
-    infectionsByRequestedTime = currentlyInfected * Math.pow (2, factor)  
-     //15% of infectionsByRequestedTime
-     const percentage = 15/100;
-     const severeCasesByRequestedTime = percentage * infectionsByRequestedTime;
 
-     const impact = {
-            currentlyInfected: currentlyInfected,
-            infectionsByRequestedTime: infectionsByRequestedTime,
-            severeCasesByRequestedTime: severeCasesByRequestedTime
-         }
-    return impact;
-}
-
-const severeImpacts = (data) =>  {
-    //currentlyInfected
-    const currentlyInfected = data.reportedCases * 50;
     //infectionsByRequestedTime
     let duration = 0;
     if(data.periodType == "days"){
@@ -52,51 +26,108 @@ const severeImpacts = (data) =>  {
     } else {
         duration = data.timeToElapse * 365;
     }
-    let factor = duration/3;
-    infectionsByRequestedTime = currentlyInfected * Math.pow (2, factor)
+    let factor = Math.floor(duration/3);
+    infectionsByRequestedTime = Math.floor(currentlyInfected * Math.pow (2, factor))
+     //15% of infectionsByRequestedTime
+     const percentage = 15/100;
+     const severeCasesByRequestedTime = Math.floor(percentage * infectionsByRequestedTime);
+
+     //numberofhospital beds
+     const occupied = 0.65 * data.totalHospitalBeds;
+     const emptyBeds = data.totalHospitalBeds - occupied;
+     const hospitalBedsByRequestedTime = Math.floor(emptyBeds - severeCasesByRequestedTime);
+
+     //casesforICU
+     casesForICUByRequestedTime = Math.floor(0.5 * infectionsByRequestedTime);
+
+     //casesforventilators
+     const casesForVentilatorsByRequestedTime = Math.floor(0.2 * infectionsByRequestedTime);
+
+     const impact = {
+            currentlyInfected: currentlyInfected,
+            infectionsByRequestedTime: infectionsByRequestedTime,
+            severeCasesByRequestedTime: severeCasesByRequestedTime,
+            hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
+            casesForICUByRequestedTime: casesForICUByRequestedTime,
+            casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime
+     }
+    return impact;
+}
+
+const severeImpacts = (data) =>  {
+    //currentlyInfected
+    const currentlyInfected = data.reportedCases * 50;
+
+    //infectionsByRequestedTime
+    let duration = 0;
+    if(data.periodType == "days"){
+        duration = data.timeToElapse;
+    } else if(data.periodType == "weeks"){
+        duration = data.timeToElapse * 7;
+    } else if(data.periodType == "months"){
+        duration = data.timeToElapse * 30;
+    } else {
+        duration = data.timeToElapse * 365;
+    }
+ 
+    let factor = Math.floor(duration/3);
+    infectionsByRequestedTime = Math.floor(currentlyInfected * Math.pow (2, factor))
 
     //15% of infectionsByRequestedTime
     const percentage = 15/100;
-    const severeCasesByRequestedTime = percentage * infectionsByRequestedTime;
+    const severeCasesByRequestedTime = Math.floor(percentage * infectionsByRequestedTime);
 
+    //numberofhospital beds
+    const occupied = 0.65 * data.totalHospitalBeds;
+    const emptyBeds = data.totalHospitalBeds - occupied;
+    const hospitalBedsByRequestedTime = Math.floor(emptyBeds - severeCasesByRequestedTime);
+
+     //casesforICU
+     casesForICUByRequestedTime = Math.floor(0.5 * infectionsByRequestedTime);
+        //casesforventilators
+        const casesForVentilatorsByRequestedTime = Math.floor(0.2 * infectionsByRequestedTime);
+    
     const severeImpact = {
             currentlyInfected: currentlyInfected,
             infectionsByRequestedTime: infectionsByRequestedTime,
-            severeCasesByRequestedTime: severeCasesByRequestedTime
+            severeCasesByRequestedTime: severeCasesByRequestedTime,
+            hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
+            casesForICUByRequestedTime: casesForICUByRequestedTime,
+            casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime
         }
      return severeImpact;   
 }
-// console.log(impacts({  
-//     region:{
-//     name: "Africa",
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71     
-//     },   
-//     periodType: "days",
-//     timeToElapse: 58,
-//     reportedCases: 674,
-//     population: 66622705,
-//     totalHospitalBeds: 1380614 } ))
+console.log(impacts({  
+    region:{
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 4,
+    avgDailyIncomePopulation: 0.73     
+    },   
+    periodType: "days",
+    timeToElapse: 38,
+    reportedCases: 2747,
+    population: 92931687,
+    totalHospitalBeds: 678874 } ))
 
-// console.log(severeImpacts({  
-//         region:{
-//         name: "Africa",
-//         avgAge: 19.7,
-//         avgDailyIncomeInUSD: 5,
-//         avgDailyIncomePopulation: 0.71     
-//         },   
-//         periodType: "days",
-//         timeToElapse:30 ,
-//         reportedCases: 674,
-//         population: 66622705,
-//         totalHospitalBeds: 1380614 } ))
+console.log(severeImpacts({  
+    region:{
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 4,
+    avgDailyIncomePopulation: 0.73     
+    },   
+    periodType: "days",
+    timeToElapse: 38,
+    reportedCases: 2747,
+    population: 92931687,
+    totalHospitalBeds: 678874 }))
 
         // const covid19ImpactEstimator = () => {
-        //    const severeImpact= severeImpacts(input);
-        //    const impact = impacts(input);
+        //    const severeImpact= severeImpacts(data);
+        //    const impact = impacts(data);
         //    const estimates = {
-        //        input,
+        //        data,
         //        estimate:{
         //        severeImpact :{
         //             currentlyInfected: severeImpact.currentlyInfected
