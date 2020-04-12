10895273fda34ce9c2bd62d1183e68d22526f2ce
TebogoMothelo <tebogo.mothelo@umuzi.org>
2020-04-12 19:42:38 +0200

    added dropdown and tester function

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,19 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+   
+}
 
-export default covid19ImpactEstimator;
+// export default covid19ImpactEstimator;
+const calculatesImpact = () => {
+    let population = document.getElementById("population").value;
+    let timeToElapse = document.getElementById("time").value;
+    let reportedCases = document.getElementById("time").value;
+    let totalHospitals = document.getElementById("time").value;
+    let PeriodType = document.getElementById("time").value;
+
+    document.getElementById("test").innerHTML = population;
+    // console.log(population)
+    // return population;
+        
+}
+
+// calculatesImpact();
